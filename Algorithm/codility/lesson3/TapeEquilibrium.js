22438b3dce63b4e51595010333637da23dc367bc
JaewooNam <namssabu12@gmail.com>
2019-09-02 15:54:52 +0900

    Change to md file

--- Algorithm/codility/lesson3/TapeEquilibrium.js
+++ Algorithm/codility/lesson3/TapeEquilibrium.js
@@ -1,14 +1,21 @@
 function solution(A) {
-  let tmp = []
-  
-  A.reduce((acc, val) => {
-    console.log('acc : ', acc)
-    console.log('val : ', val)
-    tmp.push(acc + val)
-    return acc + val
+  let totalSum = A.reduce((v, i) => {
+    return v + i
   })
-  console.log(tmp)
+
+  let i = 0
+  let leftSum = 0
+  let min = Infinity
+  while (i < A.length) {
+    totalSum -= A[i]
+    leftSum = leftSum + A[i]
+    if (min > Math.abs(totalSum - leftSum)) {
+      min = Math.abs(totalSum - leftSum)
+    }
+    i++
+  }
+
+  return min
 }
 
-const A = [3,1,2,4,3]
 solution(A)
